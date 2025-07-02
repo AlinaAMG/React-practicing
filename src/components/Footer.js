b96b77f999c7bbf33a1471a75bc55e05b44efb08
Alina <alina_gabur@yahoo.com>
2025-07-02 12:56:11 +0200

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,29 @@
-import styles from "./Footer.module.css";
+import styles from './Footer.module.css';
 
-function Footer() {
-    return (
-        <footer style={styles.footer}>
-      <p>© {new Date().getFullYear()} My Movie App. All rights reserved.</p>
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        © {new Date().getFullYear()}
+        <span> usePopcorn by Alina. All rights reserved.</span>
+      </p>
+      <p>
+        <a
+          href="https://github.com/AlinaAMG/React-practicing/tree/movieApp-frontend"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        |
+        <a
+          href="https://www.linkedin.com/in/alina-gabur-2ab539309/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </p>
     </footer>
-    )
+  );
 }
-
-export default Footer
