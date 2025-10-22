e07563908145dff0471ab49f9e95627a12164285
Alina <alina_gabur@yahoo.com>
2025-10-22 18:33:19 +0200

    bug fixed in the url

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: 'https://worldwiseproject-app.netlify.app/ ' }));
+app.use(cors({ origin: 'https://worldwiseproject-app.netlify.app' }));
 
 app.get('/cities', async (req, res) => {
   try {
